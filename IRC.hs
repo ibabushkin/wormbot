7875cf2304712d0812c9b1fa21c222331cb88638
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-01-19 20:25:04 +0100

    pondering on...

--- IRC.hs
+++ IRC.hs
@@ -1,68 +1,43 @@
+{-# GeneralizedNewtypeDeriving #-}
 module IRC where
 
 import System.IO (Handle, hPutStr)
 
-type Command = String
+newtype NickName = NickName { getNickName :: String }
+    deriving (Show, Eq)
+newtype UserName = UserName { getUserName :: String }
+    deriving (Show, Eq)
+newtype HostName = HostName { getHostName :: String }
+    deriving (Show, Eq)
+newtype Token = Token { getToken :: String }
+    deriving (Show, Eq)
+newtype Channel = Channel { getChannel :: String }
+    deriving (Show, Eq)
 
-type Origin = (String, String)
+data Command
+    = Ping Token
+    | Pong Token
+    | Nick NickName
+    | User UserName
+    | Join Channel
+    | PrivMsg Channel String
+    deriving (Show, Eq)
 
-data Message = Message { origin :: Maybe Origin
-                       , command :: Command
-                       , args :: [String]
-                       } deriving (Show, Eq)
+type Origin = (NickName, HostName)
 
--- generate a message to be send
-mkMessage :: Command -> [String] -> Message
-mkMessage = Message Nothing
+data Message = Message (Maybe Origin) Command deriving (Show, Eq)
 
--- write something to the socket in appropriate format
-write :: Handle -> Message -> IO ()
-write h msg = do let msgString = toIrc msg
-                 hPutStr h msgString
-                 putStr msgString
+-- | generate a response to a ping from the server
+pingToPong :: Command -> Maybe Command
+pingToPong (Ping t) = Just $ Pong t
+pingToPong _ = Nothing
 
--- generate nick message
-nick :: String -> Message
-nick n = mkMessage "NICK" [n]
-
--- send a nick message
-sendNick :: Handle -> String -> IO ()
-sendNick h = write h . nick
-
--- generate user message 
+{- generate user message 
 user :: String -> Message
 user n = mkMessage "USER" [n, "0", "*", "worm bot"]
 
--- send user message
-sendUser :: Handle -> String -> IO ()
-sendUser h = write h . user
-
--- generate join message
-join :: String -> Message
-join c = mkMessage "JOIN" [c]
-
--- send join message
-sendJoin :: Handle -> String -> IO ()
-sendJoin h = write h . join
-
--- generate pong message
-pong :: String -> Message
-pong t = mkMessage "PONG" [t]
-
--- send pong message
-sendPong :: Handle -> String -> IO ()
-sendPong h = write h . pong 
-
--- generate private message
-privmsg :: String -> String -> Message
-privmsg c s = mkMessage "PRIVMSG" [c, s]
-
--- send private message
-sendPrivmsg :: Handle -> String -> String -> IO ()
-sendPrivmsg h c s = write h (privmsg c s)
-
 -- format a Message to be send
-toIrc :: Message -> String
+toIrc :: Command -> String
 toIrc (Message _ c args) = c ++ ' ':argString
     where argString = unwords (init args) ++ " :" ++ last args ++ "\r\n"
 
@@ -87,5 +62,7 @@ parseIrc s = Message Nothing command args'
 -- parse the origin of a message
 parseOrigin :: String -> Maybe Origin
 parseOrigin s
-    | '!' `elem` s = Just $ break (=='!') s
+    | '!' `elem` s = Just (NickName nick, HostName host)
     | otherwise = Nothing
+    where (nick, host) = break (=='!') s
+          -}
